2aaeaf59a64a10f9e77c95cb296f827dae7589de
MARVIN SOSA SOTO <sosama@HILDEBOG.LOCAL>
2020-05-02 16:06:10 -0500

    add pipeline

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,4 +2,4 @@
 docker stop $(docker ps -a | grep "java -jar" | awk '{print $1}')
 docker rm $(docker ps -a | grep "java -jar" | awk '{print $1}')
 docker build -t custom-spring .
-docker run -p 8081:8080 --link=some-mysql -t custom-spring
+docker run -p 8081:8080 --link=some-mysql -t custom-spring &
